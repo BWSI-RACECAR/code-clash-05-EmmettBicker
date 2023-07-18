fdf64668f490dd463f13ef0fc41d4aea9c8c35ec
Emmett Bicker <88175920+EmmettBicker@users.noreply.github.com>
2023-07-18 09:10:56 -0400

    Add files via upload

--- morse.py
+++ morse.py
@@ -3,43 +3,25 @@ Copyright MIT BWSI Autonomous RACECAR Course
 MIT License
 Summer 2023
 
-Code Clash #3 - Proportional Control (propcontrol.py)
+Code Clash #5 - Morse Code Encryption(morse.py)
 
 
-Author: Chris Lai
+Author: Andrew Scott White
 
-Difficulty Level: 3/10
+Difficulty Level: 4/10
 
-Prompt: During the line following lab in Lab 2, you used 
-proportional control (among the many types of control systems) 
-to allow the RACECAR to follow a line of a specific color type. 
-In proportional control, a sensor (color camera) and actuator 
-(turning servo) work together in order to optimize a target value. 
+Prompt:Sam is trying to communicate with his friend Alfie using Morse Code, but he
+is tired of manually encrypting the messages. He has asked you to create a Python 
+script that converts messages to Morse code and vice versa. Sam’s Morse Code key is attached below. 
 
-In the line following lab, you want to ensure that the center of 
-the line contour is in the center of the screen. Given the current 
-x-axis center of the line contour's center (stored in the variable 
-center) and a tuple representing the screen resolution (stored in 
-the variable res as (x, y)), write a function that calculates the 
-steering angle (between -1 and 1, left -> right) that must be sent to the RACECAR
-in order to allow it to follow the line.
+Note: There is one space between characters and two spaces between words in Morse Code
+Hints: 	i) You can convert a message to uppercase using  message.upper()
+	ii) You can remove leading/trailing whitespace from text using text.strip()
+ 
+ 
+Morse Code Key:
 
-Constraints: The value of “center” is constrained to [res[0] >= center > 0] 
-and the resolution indicies “res[0]” and “res[1]” must be constrained 
-to [10^5 >= res[0] >= 0], [10^5 >= res[1] >= 0]. Round the output 
-answer to 6 decimal places.
-
-
-Test Cases:
-Input: center= 400, tuples (x, y) = (800, 600)  Output: 0.000000
-
-Input: center= 800, tuples(x, y) = (1600, 900)  Output: 0.000000
-
-Input: center= 720, tuples(x, y) = (1440, 900)  Output: 0.000000
-"""
-
-morse_to_string = {
-     'A':'.-', 
+'A':'.-', 
 'B':'-...',            
 'C':'-.-.', 
 'D':'-..', 
@@ -69,7 +51,7 @@ morse_to_string = {
 
 '.':'.-.-.-',                    
 '?':'..--..', 
-'!':'-.-.--',
+‘!’:’-.-.--’,
 
 '1':'.----', 
 '2':'..---', 
@@ -88,28 +70,59 @@ morse_to_string = {
 ')':'-.--.-'
 
 
-}
-class Solution:    
+Test Cases:
+Input: SOS Output: ...---...
+
+
+Input: Hello! Output: .... . .-.. .-.. --- -.-.--
+
+
+Input: HUNTER2 Output: .... ..- -. - . .-. ..---
+"""
+
+
+##########################################################################
+# MORSE DICTIONARY
+##########################################################################
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', '.':'.-.-.-', '?':'..--..', '!':'-.-.--', 
+                    '/':'-..-.', '-':'-....-', '(':'-.--.', ')':'-.--.-'}
+
+
+##########################################################################
+# Function to encrypt the string
+# according to MORSE_CODE_DICT
+##########################################################################
+
+class Solution:  
     def encrypt(self, message):
             out = ""
             for char in message:
                  if char == " ":
                     out += " "
                  else:
-                    out = out + morse_to_string[char]
+                    out = out + MORSE_CODE_DICT[char]
         
             return out[:-1]
 
+
 def main():
-    center = int(input().strip())
-    resx = int(input().strip())
-    resy = int(input().strip())
-    res = (resx, resy)
+     str1=input()
+     tc1= Solution()
+     ans=tc1.encrypt(str1)
+     print(ans)
 
-    tc1 = Solution()
-    ans = tc1.propcontrol(center, res)
-    ans=format(ans, ".6f")
-    print(ans)
-
-if __name__ and "__main__":
+if __name__ == '__main__':
     main()
